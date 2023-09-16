5a16e01c4f7bfa32f8665fc4cd4a6d384081544d
Akhilesh Moorthy <akhileshmoorthy2010@gmail.com>
2023-09-15 23:21:57 -0700

    Boundary Box, moved the capsule to the left, added more planets

--- Assets/Scripts/SpaceShipControl.cs
+++ Assets/Scripts/SpaceShipControl.cs
@@ -4,19 +4,27 @@ public class SpaceShipControl : MonoBehaviour
 {
 
     //private float speed = 10.0f;
-    //private float forwardInput;
+    private float horizontalInput;
+    float verticalInput;
+    public GameObject boundary;
+    private Rigidbody2D rb;
+
+    // Player movement speed
+    public float moveSpeed = 5f;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //forwardInput = Input.GetAxis("Horizontal");
-        //transform.Translate(Vector3.down * Time.deltaTime * speed * forwardInput);
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        //transform.Translate(Vector3.down * Time.deltaTime * speed * horizontalInput);
 
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
@@ -30,5 +38,9 @@ public class SpaceShipControl : MonoBehaviour
             position.y--;
             this.transform.position = position;
         }
+
+
+
     }
+
 }
