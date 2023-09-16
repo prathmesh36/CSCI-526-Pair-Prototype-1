56a40ced2fe217f73038db9c0cedbf012bd37751
Akhilesh Moorthy <akhileshmoorthy2010@gmail.com>
2023-09-15 20:09:58 -0700

    Horizontal View, moving up and down

--- Assets/Scripts/SpaceShipControl.cs
+++ Assets/Scripts/SpaceShipControl.cs
@@ -6,8 +6,6 @@ public class SpaceShipControl : MonoBehaviour
 {
 
     private float speed = 10.0f;
-    private float turnSpeed = 10.0f;
-    private float horizontalInput;
     private float forwardInput;
 
     // Start is called before the first frame update
@@ -19,9 +17,20 @@ public class SpaceShipControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.down * Time.deltaTime * speed * forwardInput);
-        transform.Rotate(Vector3.forward, Time.deltaTime * turnSpeed * horizontalInput);
+        //forwardInput = Input.GetAxis("Horizontal");
+        //transform.Translate(Vector3.down * Time.deltaTime * speed * forwardInput);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.y++;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.y--;
+            this.transform.position = position;
+        }
     }
 }
