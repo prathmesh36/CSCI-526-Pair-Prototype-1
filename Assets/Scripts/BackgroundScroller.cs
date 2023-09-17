910c2f7887590c4f66ba4997ec3ab0988ff7c2c9
Akhilesh Moorthy <akhileshmoorthy2010@gmail.com>
2023-09-16 21:55:58 -0700

    Spawning Random Objects in Unity
    Spawning, Adding Game Manager, Modifying background scrolling code

--- Assets/Scripts/BackgroundScroller.cs
+++ Assets/Scripts/BackgroundScroller.cs
@@ -1,33 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class BackgroundScroller : MonoBehaviour
-{
-    [Range(-10f, 10f)]
-    public float scrollSpeed = 10f;
-    private float offset;
-    private Material mat;
 
-
-    private float speed = 10.0f;
-    private float forwardInput;
-
-
-    // Start is called before the first frame update
-    void Start()
+    public class BackgroundScroller : MonoBehaviour
     {
-        mat = GetComponent<Renderer>().material;
-    }
+        [Range(-10f, 10f)]
+        public float scrollSpeed = 10f;
+        private float offset;
+        private Material mat;
 
-    // Update is called once per frame
-    void Update()
-    {
-        forwardInput = Input.GetAxis("Horizontal");
-        offset += (Time.deltaTime * scrollSpeed);
 
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.RightArrow)) {
-            offset += (2 * forwardInput);
+        private float speed = 10.0f;
+        private float forwardInput;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            mat = GetComponent<Renderer>().material;
         }
 
-        mat.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+        // Update is called once per frame
+        void Update()
+        {
+            forwardInput = Input.GetAxis("Horizontal");
+            offset += (Time.deltaTime * scrollSpeed);
+
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.RightArrow))
+            {
+                offset += (2 * forwardInput);
+            }
+
+            mat.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+        }
     }
-}
+
